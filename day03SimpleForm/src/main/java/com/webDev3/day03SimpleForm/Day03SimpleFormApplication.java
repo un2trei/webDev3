217418cdfcb82acd377f23dc997300097d7536f8
alex <alex.coanda@gmail.com>
2023-04-26 14:12:54 -0400

    spring boot app start + git testing

---
+++ day03SimpleForm/src/main/java/com/webDev3/day03SimpleForm/Day03SimpleFormApplication.java
@@ -0,0 +1,13 @@
+package com.webDev3.day03SimpleForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day03SimpleFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day03SimpleFormApplication.class, args);
+	}
+
+}
---
+++ day03SimpleForm/src/test/java/com/webDev3/day03SimpleForm/Day03SimpleFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webDev3.day03SimpleForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day03SimpleFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
